686064580cefdb92f75da2a84c8a54e04f1ce54d
luren <lurenjia534@outlook.com>
2025-03-01 21:52:55 +0800

    feat: Add MSAL dependency for Microsoft identity authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
+    // msal android
+    implementation("com.microsoft.identity.client:msal:5.10.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
