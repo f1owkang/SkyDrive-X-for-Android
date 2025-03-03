e47a8bee3c796a25d03733254aa410749488952c
luren <lurenjia534@outlook.com>
2025-03-03 11:10:07 +0800

    feat: Implement HiltWorkerFactory and set up WorkManager configuration
    This commit introduces `HiltWorkerFactory` to enable dependency injection for WorkManager workers and configures WorkManager within the `MyApplication` class. Key changes include:
    
    -   **HiltWorkerFactory:** Implemented `HiltWorkerFactory` to manage worker creation and inject dependencies.
    -   **WorkManager Configuration:** Configured WorkManager within `MyApplication` to use the `HiltWorkerFactory`, ensuring that workers can receive injected dependencies.

--- app/src/main/java/com/lurenjia534/nextonedrivev3/MyApplication.kt
+++ app/src/main/java/com/lurenjia534/nextonedrivev3/MyApplication.kt
@@ -1,11 +1,23 @@
 package com.lurenjia534.nextonedrivev3
 
 import android.app.Application
+import androidx.work.Configuration
+import com.lurenjia534.nextonedrivev3.workers.HiltWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApplication : Application() {
+class MyApplication : Application(), Configuration.Provider {
+    
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    
     override fun onCreate() {
         super.onCreate()
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
